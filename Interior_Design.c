095888c3ff2161d52189310f4a70bc5fe36a661e
sriram78747 <147419201+sriram78747@users.noreply.github.com>
2023-10-17 10:25:24 +0530

    #include<stdio.h> int main() {     int a,b,c,d;     scanf("%d%d%d%d",&a,&b,&c,&d);     if(a+b>=c+d)     printf("%d",c+d);     else if(a+b<c+d)     printf("%d",a+b);
    }

---
+++ Interior_Design.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    if(a+b>=c+d)
+    printf("%d",c+d);
+    else if(a+b<c+d)
+    printf("%d",a+b);
+    
+}
