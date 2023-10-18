3d11bab39aa270b1c7507ebb3da3cddb39914850
sriram78747 <147419201+sriram78747@users.noreply.github.com>
2023-10-18 10:51:13 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     int ans=ceil(n/4.0);     printf("%d",ans); }

--- Cars.c
+++ Cars.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-int n;
-scanf("%d",&n);
-if(n%4==0)
-printf("%d",n/4);
-else
-printf("%d",n/4+1);
+    int n;
+    scanf("%d",&n);
+    int ans=ceil(n/4.0);
+    printf("%d",ans);
 }
