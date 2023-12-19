a095c582f883eac8daa5fcf758d163ebe6152b90
sriram78747 <147419201+sriram78747@users.noreply.github.com>
2023-12-19 11:21:32 +0530

    #include<stdio.h> int main() {     int i,n,flag=1,odd;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);          if(arr[i]%2!=0&&i%2==0)          {              flag=0;              break;          }     }
        if(flag==1)
        {
            printf("True");
        }
        else
        {
            printf("False");
        }
    }

---
+++ Strictly_ODD.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,flag=1,odd;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+         if(arr[i]%2!=0&&i%2==0)
+         {
+             flag=0;
+             break;
+         }
+    }
+    
+    if(flag==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
