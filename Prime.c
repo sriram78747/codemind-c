799b38abff620a95b20792d0ec219ee10bb01a12
sriram78747 <147419201+sriram78747@users.noreply.github.com>
2024-02-21 09:46:33 +0530

    #include<stdio.h> int main() {     int i,n,count=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             count++;         }     }     if(count==2)     {         printf("Prime");     }     else     {         printf("Not Prime");     } }

--- Prime.c
+++ Prime.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,count=0;
+    int i,n,count=0;
     scanf("%d",&n);
-    for (i=1;i<=n;i++)
+    for(i=1;i<=n;i++)
     {
         if(n%i==0)
         {
